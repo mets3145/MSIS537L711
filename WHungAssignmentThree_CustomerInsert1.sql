19b9471794bde89277a847793aa0cf1393f99c3a
mets3145 <william.hung2@marist.edu>
2015-09-18 21:09:01 -0400

    AssignmentTwo and Three
    Files related to Assignment Two and Assignment Three

---
+++ WHungAssignmentThree_CustomerInsert1.sql
@@ -0,0 +1,26 @@
+--Insert Customer values
+
+
+--Sheri Gordan--
+Insert Into CUSTOMER
+Values ('C0954327','Sheri','Gordon','Littleton','CO','80129-5543','$230.00');
+
+--Jim Glussman--
+Insert Into CUSTOMER
+Values ('C1010398','Jim','Glussman','Denver','CO','80111-0033','$200.00');
+
+--Beth Taylor--
+Insert Into CUSTOMER
+Values ('C2388597','Beth','Taylor','Seattle','WA','98103-1121','$500.00');
+
+--Betty Wise--
+Insert Into CUSTOMER
+Values ('C3340959','Betty','Wise','Seattle','WA','98178-3311','$200.00');
+
+--Bob Mann--
+Insert Into CUSTOMER
+Values ('C3499503','Bob','Mann','Monroe','WA','98013-1095','$0.00');
+
+--Ron Thompson--
+Insert Into CUSTOMER
+Values ('C8543321','Ron','Thompson','Renton','WA','98666-1289','$85.00');
---
+++ WHungAssignmentThree_EmployeeInsert2.sql
@@ -0,0 +1,25 @@
+--Insert Employee Values
+
+--Landi Santos
+Insert into Employee
+Values ('E1329594','Landi','Santos','(303)789-1234','LStantos@bigco.com');
+
+--Joe Jenkins
+Insert into Employee
+Values ('E8544399','Joe','Jenkins','(303)221-9875','JJenkins@bigco.com');
+
+--Amy Tang
+Insert into Employee
+Values ('E8843211','Amy','Tang','(303)556-4321','ATang@bigco.com');
+
+--Colin White
+Insert into Employee
+Values ('E9345771','Colin','White','(303)221-4453','CWhite@bigco.com');
+
+--Thomas Johnson
+Insert into Employee
+Values ('E9884325','Thomas','Johnson','(303)556-9987','TJohnson@bigco.com');
+
+--Mary Hill
+Insert into Employee
+Values ('E9954302','Mary','Hill','(303)556-9871','MHill@bigco.com');
